4368c66ea768fc2bea1a2dfa0b7980e56b047930
ernestolindo <2288667@clases.edu.sv>
2024-09-30 20:19:25 -0600

    Iniciamos proyecto auth-app

---
+++ React/auth-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./assets/css/App.css";
+import { Session } from "./assets/css/pages/session/Session";
+
+function App() {
+  return (
+    <>
+      <Session />
+    </>
+  );
+}
+
+export default App;
---
+++ React/auth-app/src/assets/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgTNVtRi8hZWGDD_m1bHQRPSV6qBe5YBM",
+  authDomain: "auth-app-56d0c.firebaseapp.com",
+  projectId: "auth-app-56d0c",
+  storageBucket: "auth-app-56d0c.appspot.com",
+  messagingSenderId: "452869435881",
+  appId: "1:452869435881:web:062ec68e360a7d5c389b78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
