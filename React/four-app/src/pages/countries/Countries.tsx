29a3b35b0c4c9d3452897dd7a0bef178a5991d15
ernestolindo <2288667@clases.edu.sv>
2024-10-16 22:06:23 -0600

    Repaso de React, pero usando TS 1

---
+++ React/four-app/src/pages/countries/Countries.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { ICountry } from "../../models/ICountry";
+
+const APIuri = "https://restcountries.com/v3.1/all";
+
+export const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  const getCountries = async () => {
+    const response: Response = await fetch(APIuri);
+    const data: ICountry[] = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  return <div>Countries</div>;
+};
